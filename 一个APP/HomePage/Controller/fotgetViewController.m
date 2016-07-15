990ca0f182e18471cf808af75205872cbc9e546e
GYF321 <32176573@qq.com>
2016-07-15 15:23:46 +0800

    登录界面跳转以及搭建全部完成

---
+++ 一个APP/HomePage/Controller/fotgetViewController.m
@@ -0,0 +1,37 @@
+//
+//  fotgetViewController.m
+//  一个APP
+//
+//  Created by 远古科技 on 16/7/13.
+//  Copyright © 2016年 llb. All rights reserved.
+//
+
+#import "fotgetViewController.h"
+
+@interface fotgetViewController ()
+
+@end
+
+@implementation fotgetViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
