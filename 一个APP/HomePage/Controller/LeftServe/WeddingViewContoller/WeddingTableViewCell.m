920ec8fa3cea69c8958cc208dd9ec17a08b7f615
llb <745680899@qq.com>
2016-09-10 18:15:01 +0800

    婚庆占位图

--- 一个APP/HomePage/Controller/LeftServe/WeddingViewContoller/WeddingTableViewCell.m
+++ 一个APP/HomePage/Controller/LeftServe/WeddingViewContoller/WeddingTableViewCell.m
@@ -24,7 +24,7 @@ static NSString *headUrl = @"139.129.209.189:8080/shangcheng";
     self.numLabel.text = [NSString stringWithFormat:@"%@分", model.shangjiaPingfen];
     self.placeLabel.text = model.shangjiaWeizhi;
     NSString *imageUrl = [NSString stringWithFormat:@"%@%@", headUrl, model.shangjiaTouxiang];
-    [self.weddingImage sd_setImageWithURL:[NSURL URLWithString:imageUrl] placeholderImage:[UIImage imageNamed:@"zhanwei"]];
+    [self.weddingImage sd_setImageWithURL:[NSURL URLWithString:imageUrl] placeholderImage:[UIImage imageNamed:@"zhanweitouxiang"]];
     self.piceLabel.text = [NSString stringWithFormat:@"%@折起", model.shangjiaZhekou];
 }
 
